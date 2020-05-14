9d4ff65a6975e3fc0e5b71572d70396676865ca8
Tymon Jakubowski <tymon.jakubowski@gmail.com>
2020-05-14 18:40:15 +0200

    Remove Debug folder from repo

--- code/Debug/Lib/ST7735S/Src/font16.d
+++
@@ -1,4 +0,0 @@
-Lib/ST7735S/Src/font16.o: ../Lib/ST7735S/Src/font16.c \
- D:/Programowanie/STM/ptm-machine/code/Lib/ST7735S/Inc/fonts.h
-
-D:/Programowanie/STM/ptm-machine/code/Lib/ST7735S/Inc/fonts.h:
--- code/Debug/Lib/ST7735S/Src/font8.d
+++
@@ -1,4 +0,0 @@
-Lib/ST7735S/Src/font8.o: ../Lib/ST7735S/Src/font8.c \
- D:/Programowanie/STM/ptm-machine/code/Lib/ST7735S/Inc/fonts.h
-
-D:/Programowanie/STM/ptm-machine/code/Lib/ST7735S/Inc/fonts.h:
